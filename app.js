743980a799fe879e26d770e3c268c5a096ef8240
Mahendra singh <mahi07ms03@gmail.com>
2024-01-18 23:25:37 +0530

    middleware

--- app.js
+++ app.js
@@ -1,47 +1,41 @@
-const fs = require('fs');
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
+
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
+
+// MiddleWares
+app.use(morgan('dev'));
 app.use(express.json());
-// app.get('/',(req,res)=>{
-//     res
-//     .status(404)
-//     .json({message:'Hello From the server side!.....',app:'Natours'});
-// });
 
-// app.post('/',(req,res)=>{
-//     res.send('You can post to this URL!!');
-// })
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
-app.get('/api/v1/tours',(req,res) =>{
-    res.status(200).json({
-        status:'success',
-        results:tours.length,
-        data:{
-            tours
-        }
-    });
+app.use((req,res,next)=>{
+    console.log('Hello From the middleWare👋');
+    next();
+});
+app.use((req,res,next)=>{
+    // console.log('Hello From the middleWare👋');
+    req.requestTime = new Date().toISOString();
+    next();
 });
 
-app.post('/api/v1/tours',(req,res)=>{
-    // console.log(req.body);
-    const newId = tours[tours.length-1].id+1;
-    const newTour =  Object.assign({id:newId},req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
-        res.status(201).json({
-            status:'success',
-            data:{
-                tour:newTour
-            }
-        });
-    }
-    );
-});
 
-const port = 3000;
-app.listen(port,()=>{
-    console.log(`App running on ${port} port!`);
-})
+// 2) Route Handlers
+
+
+// app.get('/api/v1/tours',getAllTours);
+// app.get('/api/v1/tours/:id',getTour);
+// app.post('/api/v1/tours',createTour);
+// app.patch('/api/v1/tours/:id',updateTour);
+// app.delete('/api/v1/tours/:id',deleteTour);
+
+// 3) Routes
+
+
+
+    app.use('/api/v1/tours',tourRouter);
+    app.use('/api/v1/users',userRouter);
+
+
+  // 4) Start the Server
+module.exports = app;
