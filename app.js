4d09b525accb3f99c13fdc04eff608b29453d568
Mahendra singh <mahi07ms03@gmail.com>
2024-01-20 02:24:21 +0530

    eslint

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
 // MiddleWares
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req,res,next)=>{
     console.log('Hello From the middleWare👋');
